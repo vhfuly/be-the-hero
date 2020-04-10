436587c276467c35a09b786e53f739db2c8070e1
Victor Hugo Fuly <victor_hugo_fuly@hotmail.com>
2020-04-10 19:50:10 -0300

    front-end do ap

--- backend/scr/controllers/IncidentController.js
+++ backend/scr/controllers/IncidentController.js
@@ -23,7 +23,7 @@ module.exports={
         response.header('X-Total-Count',count['count(*)']);
         return response.json(incidents);
     },
-
+ 
     async create(request, response){
         const {title, description, value} = request.body;
         const ong_id = request.headers.authorization;
