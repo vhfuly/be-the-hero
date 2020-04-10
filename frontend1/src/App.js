c465d32d93226896403e7b671f0fedf5248dce14
Victor Hugo Fuly <62678105+vhfuly@users.noreply.github.com>
2020-04-10 20:24:37 -0300

    Add files via upload

---
+++ frontend1/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './global.css'
+
+import Routes from './routes';
+// JSX  (Javascript xml)
+
+function App() {
+ 
+ 
+  return (
+    <div>
+    <Routes/>
+  </div>
+  )
+}
+
+
+
+export default App;
