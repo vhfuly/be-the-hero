2d65298821ceaf0c5143c756b5a9e212412a1070
Victor Hugo Fuly <62678105+vhfuly@users.noreply.github.com>
2020-04-10 20:36:13 -0300

    Add files via upload

---
+++ frontend1/build/precache-manifest.719945a8528b1e97b4b696e4e3a3a1e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77136ac2dfd873421ed5b64f93a9d8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b81fe804de33a79014e8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3af1fd386a9358cd718",
+    "url": "/static/js/2.6f67e866.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f67e866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b81fe804de33a79014e8",
+    "url": "/static/js/main.4c82a669.chunk.js"
+  },
+  {
+    "revision": "49d54895f917a5349f7d",
+    "url": "/static/js/runtime-main.c80e4867.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
