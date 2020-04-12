5543bbd272a56caf381206bb73b1dc9ba2aa19d2
Victor Hugo Fuly <62678105+vhfuly@users.noreply.github.com>
2020-04-12 00:04:38 -0300

    Add files via upload

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './scr/routes';
+
+//container usamos tag view e para texto a tag text.
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
